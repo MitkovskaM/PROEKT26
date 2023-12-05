35e20fc3680a9322631bc2ea18598f6cb91310a5
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-12-05 13:45:32 +0100

    Add new java proekt

---
+++ src/zadaca25/Student.java
@@ -0,0 +1,26 @@
+package zadaca25;
+
+public class Student {
+int id;
+String ime;
+int godini;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id=id;
+}
+public String getIme() {
+	return ime;
+}
+public void setIme(String ime) {
+	this.ime=ime;
+}
+public int getGodini() {
+	return godini;
+}
+public void  setGodini(int godine) {
+	this.godini=godini;
+} } 
+
+	
---
+++ src/zadaca25/studenti.java
@@ -0,0 +1,15 @@
+package zadaca25;
+
+public class studenti {
+public static void main(String []args) {
+	Student marko=new Student();
+	
+	marko.setId(1);;
+	marko.setIme("Marko");
+	marko.setGodini(18);
+	
+	System.out.println("Reden broj "+ marko.getId()+" e studentot "+marko.getIme()+ " star " + marko.getGodini() +" godini.");
+
+}
+
+}
